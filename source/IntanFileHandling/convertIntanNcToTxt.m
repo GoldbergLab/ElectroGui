1cf23cf126d9b77e0c3baa00edc805432c031592
Brian Kardon <brian.kardon@gmail.com>
2021-04-21 00:30:11 -0400

    Added documentation and some extra optional function parameters to fine tune behavior

--- source/IntanFileHandling/convertIntanNcToTxt.m
+++ source/IntanFileHandling/convertIntanNcToTxt.m
@@ -1,22 +1,69 @@
-function convertIntanNcToTxt(pathToNc)
+function convertIntanNcToTxt(pathToNcs, recursive, regex)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% convertIntanNcToTxt: A function for converting new .nc binary channel 
+%   files to Intan legacy .txt channel files. This is really only useful
+%   for verifying that the txt=>nc converter is working correctly, and
+%   debugging if not.
+%
+% usage:  
+%   convertIntanNcToTxt(pathToNcs)
+%   convertIntanNcToTxt(pathToNcs, recursive)
+%   convertIntanNcToTxt(pathToNcs, recursive, regex)
+%
+% where,
+%    pathToNcs is a char array representing a path to either a single .nc
+%       file, or a directory containing them.
+%    recursive is an optional boolean flag indicating whether or not to 
+%       look in subdirectories. You can also specify a positive integer 
+%       indicating how many levels deep to look. Default is true.
+%    regex is an optional char array representing a regular expression to
+%       use to filter the files found. Default is '.*\.[Nn][Cc]$'
+%
+% See also: convertIntanTxtToNc, intan_converter_to_binary_channel_files
+%
+% Version: <version>
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-data = readIntanNcFile(pathToNc);
-[path, name, ~] = fileparts(pathToNc);
-pathToTxt = fullfile(path, [name, '.txt']);
+if ~exist('regex', 'var')
+    regex = '.*\.[Nn][Cc]$';
+end
+if ~exist('recursive', 'var')
+    recursive = true;
+end
 
-year = data.time(1);
-month = data.time(2);
-day = data.time(3);
-hour = data.time(4);
-minute = data.time(5);
-second = double(data.time(6))+double(data.time(7))/1000000;
-date = sprintf('%02u/%02u/%d', month, day, year);
-time = sprintf('%02u:%02u:%f', hour, minute, second);
+if exist(pathToNcs, 'dir')
+    pathToNcs = findFilesByRegex(pathToNcs, regex, false, recursive);
+elseif exist(pathToNcs, 'file')
+    pathToNcs = {pathToNcs};
+else
+    error('''%s'' is not a valid file or directory.', pathToNcs);
+end
 
-fileID = fopen(pathToTxt,'w');
-fprintf(fileID,'%s\t%s\r\n', date, time);
-fprintf(fileID,'%s\r\n', data.metaData);
-fprintf(fileID,'%s%f\r\n\r\n', 'delta_t = ', data.dt);
-fprintf(fileID,'%f\r\n', data.data);
+fprintf('Found %d nc files to convert. Converting...\n', length(pathToNcs));
+for k = 1:length(pathToNcs)
+    pathToNc = pathToNcs{k};
+    
+    data = readIntanNcFile(pathToNc);
+    [path, name, ~] = fileparts(pathToNc);
+    pathToTxt = fullfile(path, [name, '.txt']);
 
-fclose(fileID);
+    year = data.time(1);
+    month = data.time(2);
+    day = data.time(3);
+    hour = data.time(4);
+    minute = data.time(5);
+    second = double(data.time(6))+double(data.time(7))/1000000;
+    date = sprintf('%02u/%02u/%d', month, day, year);
+    time = sprintf('%02u:%02u:%f', hour, minute, second);
+
+    fileID = fopen(pathToTxt,'w');
+    fprintf(fileID,'%s\t%s\r\n', date, time);
+    fprintf(fileID,'%s\r\n', data.metaData);
+    fprintf(fileID,'%s%f\r\n\r\n', 'delta_t = ', data.dt);
+    fprintf(fileID,'%f\r\n', data.data);
+
+    fclose(fileID);
+end
--- source/IntanFileHandling/convertIntanTxtToNc.m
+++ source/IntanFileHandling/convertIntanTxtToNc.m
@@ -1,36 +1,86 @@
-function convertIntanTxtToNc(pathToTxt)
+function convertIntanTxtToNc(pathToTxts, recursive, regex)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% convertIntanNcToTxt: A function for converting Intan legacy .txt channel
+%   files to new .nc binary channel files.
+% usage:  
+%   convertIntanNcToTxt(pathToNcs)
+%   convertIntanNcToTxt(pathToNcs, recursive)
+%   convertIntanNcToTxt(pathToNcs, recursive, regex)
+%
+% where,
+%    pathToTxts is a char array representing a path to either a single .txt
+%       file, or a directory containing them.
+%    recursive is an optional boolean flag indicating whether or not to 
+%       look in subdirectories. You can also specify a positive integer 
+%       indicating how many levels deep to look. Default is true.
+%    regex is an optional char array representing a regular expression to
+%       use to filter the files found. Default is '.*\.[Tt][Xx][Tt]$'
+%
+% This function can be used to convert old .txt Intan channel files (which
+%   were created from .rhd files), to the new .nc format. This function can
+%   work on a single file, or it can be given a directory and will work on
+%   all the .txt files within the directory.
+%
+% See also: convertIntanNcToTxt, intan_converter_to_binary_channel_files
+%
+% Version: <version>
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[path, name, ~] = fileparts(pathToTxt);
-chanTxt = regexp(name, 'chan([0-9]+)$', 'tokens');
-if isempty(chanTxt)
-    warning('Could not extract channel number from path name - defaulting to channel 0.');
-    channel = 0;
-else
-    channel = str2double(chanTxt{1});
+if ~exist('regex', 'var')
+    regex = '.*\.[Tt][Xx][Tt]$';
+end
+if ~exist('recursive', 'var')
+    recursive = true;
 end
 
-fs = 20000;
-delimiterIn = ' ';
-headerLinesIn = 3;
-dateTimeLine = 1;
-metaDataLine = 2;
-deltaTLine = 3;
+if exist(pathToTxts, 'dir')
+    pathToTxts = findFilesByRegex(pathToTxts, regex, false, recursive);
+elseif exist(pathToTxts, 'file')
+    pathToTxts = {pathToTxts};
+else
+    error('''%s'' is not a valid file or directory.', pathToTxts);
+end
 
-A = importdata(pathToTxt,delimiterIn,headerLinesIn);
-textData = A.textdata;
-data = A.data;
-% Split timestamp text on '.' symbol, since datevec can't handle
-%   microseconds
-dateTimeParts = strsplit(textData{dateTimeLine}, '.');
-dateTimeString = dateTimeParts{1};
-timeStampVector = datevec(dateTimeString, 'mm/dd/yyyy	HH:MM:SS');
-% Add in microseconds value
-timeStampVector = [timeStampVector, str2double(dateTimeParts{2})];
+fprintf('Found %d txt files to convert. Converting...\n', length(pathToTxts));
+for k = 1:length(pathToTxts)
+    pathToTxt = pathToTxts{k};
+    
+    [path, name, ~] = fileparts(pathToTxt);
+    chanTxt = regexp(name, 'chan([0-9]+)$', 'tokens');
+    if isempty(chanTxt)
+        warning('Could not extract channel number from path name - defaulting to channel 0.');
+        channel = 0;
+    else
+        channel = str2double(chanTxt{1});
+    end
 
-deltaTString = regexp(textData(deltaTLine), '[0-9]?\.?[0-9]+', 'match');
-deltaT = str2double(deltaTString{1});
-metaData = textData{metaDataLine};
+    fs = 20000;
+    delimiterIn = ' ';
+    headerLinesIn = 3;
+    dateTimeLine = 1;
+    metaDataLine = 2;
+    deltaTLine = 3;
 
-newPath = fullfile(path, [name, '.nc']);
+    A = importdata(pathToTxt,delimiterIn,headerLinesIn);
+    textData = A.textdata;
+    data = A.data;
+    % Split timestamp text on '.' symbol, since datevec can't handle
+    %   microseconds
+    dateTimeParts = strsplit(textData{dateTimeLine}, '.');
+    dateTimeString = dateTimeParts{1};
+    timeStampVector = datevec(dateTimeString, 'mm/dd/yyyy	HH:MM:SS');
+    % Add in microseconds value
+    timeStampVector = [timeStampVector, str2double(dateTimeParts{2})];
 
-writeIntanNcFile(newPath, timeStampVector, deltaT, channel, metaData, data, true);
+    deltaTString = regexp(textData(deltaTLine), '[0-9]?\.?[0-9]+', 'match');
+    deltaT = str2double(deltaTString{1});
+    metaData = textData{metaDataLine};
+
+    newPath = fullfile(path, [name, '.nc']);
+
+    writeIntanNcFile(newPath, timeStampVector, deltaT, channel, metaData, data, true);
+    fprintf('\tCompleted %d of %d\n', k, length(pathToTxts));
+end
