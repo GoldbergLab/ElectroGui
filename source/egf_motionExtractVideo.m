232173ea88b30ddb54ce07422825a424d7764b21
Brian Kardon <brian.kardon@gmail.com>
2022-04-28 01:23:32 -0400

    Improved motionExtractVideo filter

--- source/egf_motionExtractVideo.m
+++ source/egf_motionExtractVideo.m
@@ -13,15 +13,23 @@ end
 %     params.intensity = 1.0;   % Intensity of de-median operation, from 0 to 1. 0 leaves the video unchanged, 1 completely subtracts the median frame.
 % end
 
-diffKernel = permute([-1, 1], [4, 3, 2, 1]);
+% diffKernel = permute([-1, 1], [4, 3, 2, 1]);
+% 
+% motion = abs(convn(mean(video, 3), diffKernel, 'same'));
 
-motion = abs(convn(squeeze(mean(video, 3)), diffKernel, 'same'));
-meanMotion = mean(motion(:));
-stdevMotion = std(motion(:));
+videoD = double(mean(video, 3));
 
-motion = (motion - (meanMotion - stdevMotion)) / (2*stdevMotion);
-motion(motion < 0) = 0;
-motion(motion > 1) = 1;
-motion = motion .* motion;
+motion = padarray(videoD(:, :, :, 2:end) - videoD(:, :, :, 1:end-1), [0, 0, 0, 1], 'post');
+
+% meanMotion = mean(motion(:));
+% stdevMotion = std(motion(:));
+% 
+% motion = (motion - (meanMotion - stdevMotion)) / (2*stdevMotion);
+% motion(motion < 0) = 0;
+% motion(motion > 1) = 1;
+maxMotion = max(motion(:));
+minMotion = min(motion(:));
+motion = (motion - minMotion) / (maxMotion - minMotion);
+% motion = motion .* motion;
 motion = cat(3, motion, motion, motion);
-video = uint8(double(video) .* motion);
+video = uint8(256 * motion); %videoD .* motion);
