aed6ccf39395242270f37bb789103c6e173efdd5
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2023-06-15 09:50:44 -0400

    Finished compareIntanNcAndTxt

--- source/IntanFileHandling/compareIntanNcAndTxt.m
+++ source/IntanFileHandling/compareIntanNcAndTxt.m
@@ -1,30 +1,62 @@
-function pairs = compareIntanNcAndTxt(rootDir)
+function [pairs, unpairedNcFiles, unpairedTxtFiles] = compareIntanNcAndTxt(rootDir)
 
 ncFiles = findFilesByRegex(rootDir, '.*\.[nNcC]');
 txtFiles = findFilesByRegex(rootDir, '.*\.[tTxXtT]');
 
 pairs = struct.empty();
 
+unpairedNcFiles = {};
+unpairedTxtFiles = {};
+
+pairedTxtIdx = [];
+
 for k = 1:length(ncFiles)
     ncFile = ncFiles{k};
     [~, ncFileName, ~] = fileparts(ncFile);
+    foundMatch = false;
     for j = 1:length(txtFiles)
+        if find(pairedTxtIdx==j, 1)
+            % Make sure we don't match a txt file to a nc file twice
+            continue
+        end
         txtFile = txtFiles{j};
         [~, txtFileName, ~] = fileparts(txtFile);
         if strcmp(ncFileName, txtFileName)
             idx = length(pairs)+1;
             pairs(idx).ncFile = ncFile;
             pairs(idx).txtFile = txtFile;
+            foundMatch = true;
+            pairedTxtIdx(end+1) = j;
+            break
         end
+    end
+    if ~foundMatch
+        unpairedNcFiles{end+1} = ncFile;
     end
 end
 
-minAccuracy = eps(single(0))
+for j = 1:length(txtFiles)
+    txtFile = txtFiles{j};
+    [~, txtFileName, ~] = fileparts(txtFile);
+    foundMatch = false;
+    for k = 1:length(ncFiles)
+        ncFile = ncFiles{k};
+        [~, ncFileName, ~] = fileparts(ncFile);
+        if strcmp(ncFileName, txtFileName)
+            foundMatch = true;
+            break
+        end
+    end
+    if ~foundMatch
+        unpairedTxtFiles{end+1} = txtFile;
+    end
+end
+
 
 for k = 1:length(pairs)
     displayProgress('Compared %d of %d\n', k, length(pairs), 20)
-    [ncData, ncFs, ncDateandtime, ncLabel, ncProps] = egl_Intan_Nc(pairs(k).ncFile, true);
-    [txtData, txtFs, txtDateandtime, txtLabel, txtProps] = egl_HC_ad(pairs(k).txtFile, true);
+    [ncData, ncFs, ncDateandtime, ncLabel, ~] = egl_Intan_Nc(pairs(k).ncFile, true);
+    [txtData, txtFs, txtDateandtime, txtLabel, ~] = egl_HC_ad(pairs(k).txtFile, true);
     match = true;
     matchErrors = {};
     
