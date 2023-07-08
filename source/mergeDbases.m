bdbb2e13293df65eafe482b96643c88a208e5abb
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2023-07-07 21:37:01 -0400

    Updated mergeDbases to accept more than two dbases

--- source/mergeDbases.m
+++ source/mergeDbases.m
@@ -1,18 +1,19 @@
-function dbase = mergeDbases(dbaseA, dbaseB)
+function dbase = mergeDbases(varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% mergeDbases: Merge two dbases.
-% usage:  dbase = mergeDbases(dbaseA, dbaseB)
+% mergeDbases: Merge multiple dbases.
+% usage:  dbase = mergeDbases(newDbase, dbase, ..., dbaseN)
 %
 % where,
-%    dbaseA is the first dbase
-%    dbaseB is the second dbase
+%    newDbase is the first dbase
+%    dbase is the second dbase
+%    dbaseN is the nth dbase
 %
 % electro_gui produces a dbase struct that contains a variety of data about
-%   an array of data files. This function can be used to merge two dbases.
+%   an array of data files. This function can be used to merge dbases.
 %
 % Note that this does not currently do anything sophisticated if there are
-%   conflicting values that can't be merged in the two dbases - it just
-%   defaults to the value from the first dbase supplied (dbaseA). This is a
+%   conflicting values that can't be merged in the dbases - it just
+%   defaults to the value from the first dbase supplied (newDbase). This is a
 %   pretty blunt tool at the moment that mostly just merges the file lists.
 %
 % See also: electro_gui
@@ -24,44 +25,46 @@ function dbase = mergeDbases(dbaseA, dbaseB)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % If user provides a path, use it to load the dbase
-if ischar(dbaseA)
-    s = load(dbaseA, 'dbase');
-    dbaseA = s.dbase;
+for k = nargin:-1:1
+    if ischar(varargin{k})
+        s = load(newDbase, 'dbase');
+        newDbase = s.dbase;
+    else
+        newDbase = varargin{k};
+    end
+
+    if k == nargin
+        dbase = newDbase;
+        continue;
+    end
+    
+    dbase.PathName = newDbase.PathName;
+    dbase.Times = [newDbase.Times, dbase.Times];
+    dbase.FileLength = [newDbase.FileLength, dbase.FileLength];
+    dbase.SoundFiles = [newDbase.SoundFiles; dbase.SoundFiles];
+    for k = 1:length(newDbase.ChannelFiles)
+        dbase.ChannelFiles{k} = [newDbase.ChannelFiles{k}; dbase.ChannelFiles{k}];
+    end
+    dbase.SoundLoader = newDbase.SoundLoader;
+    dbase.ChannelLoader = newDbase.ChannelLoader;
+    dbase.Fs = newDbase.Fs;
+    dbase.SegmentThresholds = [newDbase.SegmentThresholds, dbase.SegmentThresholds];
+    dbase.SegmentTimes = [newDbase.SegmentTimes, dbase.SegmentTimes];
+    dbase.SegmentTitles = [newDbase.SegmentTitles, dbase.SegmentTitles];
+    dbase.SegmentIsSelected = [newDbase.SegmentIsSelected, dbase.SegmentIsSelected];
+    dbase.MarkerTimes = [newDbase.MarkerTimes, dbase.MarkerTimes];
+    dbase.MarkerTitles = [newDbase.MarkerTitles, dbase.MarkerTitles];
+    dbase.MarkerIsSelected = [newDbase.MarkerIsSelected, dbase.MarkerIsSelected];
+    
+    dbase.Properties.Names = [newDbase.Properties.Names, dbase.Properties.Names];
+    dbase.Properties.Values = [newDbase.Properties.Values, dbase.Properties.Values];
+    dbase.Properties.Types = [newDbase.Properties.Types, dbase.Properties.Types];
+    
+    dbase.EventSources = newDbase.EventSources;
+    dbase.EventFunctions = newDbase.EventFunctions;
+    dbase.EventDetectors = newDbase.EventDetectors;
+    dbase.EventThresholds = newDbase.EventThresholds;
+    dbase.EventTimes = newDbase.EventTimes;
+    dbase.EventIsSelected = newDbase.EventIsSelected;
+    dbase.AnalysisState = newDbase.AnalysisState;
 end
-if ischar(dbaseB)
-    s = load(dbaseB, 'dbase');
-    dbaseB = s.dbase;
-end
-
-% Remove all data relating to the file or files given by position index fileIdx in
-% the provided dbase.
-
-dbase.PathName = dbaseA.PathName;
-dbase.Times = [dbaseA.Times, dbaseB.Times];
-dbase.FileLength = [dbaseA.FileLength, dbaseB.FileLength];
-dbase.SoundFiles = [dbaseA.SoundFiles; dbaseB.SoundFiles];
-for k = 1:length(dbaseA.ChannelFiles)
-    dbase.ChannelFiles{k} = [dbaseA.ChannelFiles{k}; dbaseB.ChannelFiles{k}];
-end
-dbase.SoundLoader = dbaseA.SoundLoader;
-dbase.ChannelLoader = dbaseA.ChannelLoader;
-dbase.Fs = dbaseA.Fs;
-dbase.SegmentThresholds = [dbaseA.SegmentThresholds, dbaseB.SegmentThresholds];
-dbase.SegmentTimes = [dbaseA.SegmentTimes, dbaseB.SegmentTimes];
-dbase.SegmentTitles = [dbaseA.SegmentTitles, dbaseB.SegmentTitles];
-dbase.SegmentIsSelected = [dbaseA.SegmentIsSelected, dbaseB.SegmentIsSelected];
-dbase.MarkerTimes = [dbaseA.MarkerTimes, dbaseB.MarkerTimes];
-dbase.MarkerTitles = [dbaseA.MarkerTitles, dbaseB.MarkerTitles];
-dbase.MarkerIsSelected = [dbaseA.MarkerIsSelected, dbaseB.MarkerIsSelected];
-
-dbase.Properties.Names = [dbaseA.Properties.Names, dbaseB.Properties.Names];
-dbase.Properties.Values = [dbaseA.Properties.Values, dbaseB.Properties.Values];
-dbase.Properties.Types = [dbaseA.Properties.Types, dbaseB.Properties.Types];
-
-dbase.EventSources = dbaseA.EventSources;
-dbase.EventFunctions = dbaseA.EventFunctions;
-dbase.EventDetectors = dbaseA.EventDetectors;
-dbase.EventThresholds = dbaseA.EventThresholds;
-dbase.EventTimes = dbaseA.EventTimes;
-dbase.EventIsSelected = dbaseA.EventIsSelected;
-dbase.AnalysisState = dbaseA.AnalysisState;
