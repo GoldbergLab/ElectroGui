ceeb0b66de473c2cbf15a62d47b1cabd78958667
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-05-07 19:21:02 -0400

    Improve error handling for egf_FIRBandPass

--- source/egf_FIRBandPass.m
+++ source/egf_FIRBandPass.m
@@ -10,6 +10,7 @@ if ischar(a) && strcmp(a, 'params')
     return
 end
 
+% If no parameters provided, use defaults
 if ~exist('params', 'var') || isempty(params)
     params = defaultParams;
 end
@@ -18,5 +19,27 @@ freq1 = str2double(params.Values{1});
 freq2 = str2double(params.Values{2});
 ord = str2double(params.Values{3});
 
-b = fir1(ord,[freq1 freq2]/(fs/2));
+% Make sure parameters were numbers
+if isnan(freq1)
+    error('Invalid frequency cutoff value: %s (must be a number)', params.Values{1});
+end
+if isnan(freq2)
+    error('Invalid frequency cutoff value: %s (must be a number)', params.Values{2});
+end
+if isnan(ord)
+    error('Invalid frequency cutoff value: %s (must be a number)', params.Values{3});
+end
+
+try
+    % Attempt to generate filter coefficients
+    b = fir1(ord,[freq1 freq2]/(fs/2));
+catch ME
+    % Frequency cutoffs out of range
+    if strcmp(ME.identifier, 'signal:fir1:FreqsOutOfRange')
+        error('Invalid frequency cutoffs for FIRBandPass for sampling rate %f: [%f, %f]. Frequencies must be between 0 and %f', fs, freq1, freq2, fs/2);
+    else
+    end
+end
+
+% Filter sound
 snd = filtfilt(b, 1, a);
