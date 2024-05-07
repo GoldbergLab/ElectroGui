6fcf972efdbb7f039feca33dddabffbea04d87cc
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-05-07 12:51:32 -0400

    Fix no default params bug in egf_FIRBandPass

--- source/egf_FIRBandPass.m
+++ source/egf_FIRBandPass.m
@@ -1,16 +1,22 @@
-function [snd lab] = egf_FIRBandPass(a,fs,params)
+function [snd, label] = egf_FIRBandPass(a, fs, params)
 % ElectroGui filter
 
-lab = 'Band-pass filtered';
-if isstr(a) & strcmp(a,'params')
-    snd.Names = {'Lower frequency','Higher frequency','Order'};
-    snd.Values = {'400','9000','80'};
+label = 'Band-pass filtered';
+defaultParams.Names = {'Lower frequency','Higher frequency','Order'};
+defaultParams.Values = {'400','9000','80'};
+
+if ischar(a) && strcmp(a, 'params')
+    snd = defaultParams;
     return
 end
 
-freq1 = str2num(params.Values{1});
-freq2 = str2num(params.Values{2});
-ord = str2num(params.Values{3});
+if ~exist('params', 'var') || isempty(params)
+    params = defaultParams;
+end
+
+freq1 = str2double(params.Values{1});
+freq2 = str2double(params.Values{2});
+ord = str2double(params.Values{3});
 
 b = fir1(ord,[freq1 freq2]/(fs/2));
 snd = filtfilt(b, 1, a);
