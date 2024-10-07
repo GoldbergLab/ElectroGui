a0b328dcf5cf949e8727f735a7117bb373830f5e
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-10-07 19:39:08 -0400

    Update egg_WhisperSeg for new multi-model web service
    Also added the ability to query web service for a list of available models by running "egg_WhisperSeg();"

--- source/egg_WhisperSeg.m
+++ source/egg_WhisperSeg.m
@@ -1,4 +1,4 @@
-function [segments, segmentNames] = egg_WhisperSeg(data, ~, ~, ~, params)
+function [segmentTimes, segmentTitles] = egg_WhisperSeg(data, ~, ~, ~, params)
 % ElectroGui segmenter
 % This segmenter uses WhisperSeg
 % (https://github.com/nianlonggu/WhisperSeg/tree/master) to segment the
@@ -6,15 +6,15 @@ function [segments, segmentNames] = egg_WhisperSeg(data, ~, ~, ~, params)
 % running either locally or remotely.
 
 % Define default segmenter parameters
-defaultParams.Names =  {'WhisperSeg hostname/IP',   'WhisperSeg service port',  'Mininum frequency (Hz)',   'Spectrogram time step (s)',    'Minimum segment length (s)',   'eps',  'Number of trials', 'Network name', 'Use labels'};
-defaultParams.Values = {'127.0.0.1',                '8050',                     '0',                        '0.0025',                       '0.01',                         '0.02', '3',                '',             'false'};
+defaultParams.Names =  {'WhisperSeg hostname/IP',       'WhisperSeg service port',  'Mininum frequency (Hz)',   'Spectrogram time step (s)',    'Minimum segment length (s)',   'eps',  'Number of trials', 'Network name', 'Use labels'};
+defaultParams.Values = {'goldbergbk.nbb.cornell.edu',   '8050',                     '0',                        '0.0025',                       '0.01',                         '0.02', '3',                'zhilei_ct2',   'false'};
 
-segmentNames = {};
+segmentTitles = {};
 
 % Check if the user passed in the string "params" instead of audio data
-if ischar(data) && strcmp(data,'params')
+if exist('data', 'var') && ischar(data) && strcmp(data,'params')
     % Return the default parameters
-    segments = defaultParams;
+    segmentTimes = defaultParams;
     return
 end
 
@@ -26,12 +26,50 @@ end
 % Extract the parameters chosen
 [host, port, min_frequency, spec_time_step, min_segment_length, eps, num_trials, network_name, use_labels] = params.Values{:};
 
+if nargin == 0
+    % If no arguments are passed, just query available models and exit
+    clear segmentTimes segmentTitles
+    % DEBUG:
+    host = 'localhost';
+    service_url = sprintf('http://%s:%s/models', host, port);
+    fprintf('Attempting to get a list of available trained WhisperSeg models host %s at port %s...\n\n', host, port)
+    try
+        options = weboptions('RequestMethod', 'POST', 'MediaType', 'application/json', 'Timeout', 5);
+        modelInfo = webwrite(service_url, '', options);
+        modelNames = modelInfo.model_names;
+        modelTimes = modelInfo.model_timestamps;
+        fprintf('Available models found:\n')
+        for k = 1:length(modelNames)
+            fprintf('\t%s (%s)\n', modelNames{k}, modelTimes{k});
+        end
+        fprintf('\nSet your WhisperSeg parameters in electro_gui accordingly.\n')
+    catch ME
+        % Request failed
+        if strcmp(ME.identifier, 'MATLAB:webservices:UnknownHost')
+            errordlg('Whisper seg web service not available at: %s', service_url);
+            error('Whisper seg web service not available at: %s\n', service_url);
+        else
+            % Something else went wrong
+            rethrow(ME);
+        end
+    end
+    return
+end
+
 % Convert numerical parameters from strings to numbers
 min_frequency = str2double(min_frequency);
 spec_time_step = str2double(spec_time_step);
 min_segment_length = str2double(min_segment_length);
 eps = str2double(eps);
 num_trials = str2double(num_trials);
+switch use_labels
+    case 'true'
+        use_labels = true;
+    case 'false'
+        use_labels = false;
+    otherwise
+        error('Invalid value for parameter use_labels: %s', use_labels)
+end
 
 % Construct a full URL from the given host name and port
 service_url = sprintf('http://%s:%s/segment/%s', host, port, network_name);
@@ -61,25 +99,24 @@ try
     response = webwrite(service_url, jsonData, options);
     % Reply received! Reformat the results into electro_gui's Nx2 segment
     % time array format
-    segments = round([response.onset, response.offset]*whisperSegFs) + 1;  % + 1 to convert from python's zero-indexing to MATLAB's one-indexing
+    segmentTimes = round([response.onset, response.offset]*whisperSegFs) + 1;  % + 1 to convert from python's zero-indexing to MATLAB's one-indexing
     if use_labels
         % Use WhisperSeg segment labels
-        segmentNames = response.cluster;
+        segmentTitles = response.cluster;
     end
-    if isfield(response, 'message') && ~isempty(response.message)
+    if isfield(response, 'message') && ~isempty(response.message) && ~strcmp(response.message, 'Success')
         % If WhisperSeg sent a message, display it in alert and command window
-
         fprintf('\nMessage from WhisperSeg server... \n\n%s\n\n ...message end\n\n.', response.message)
     end
 catch ME
     % Request failed
     if strcmp(ME.identifier, 'MATLAB:webservices:UnknownHost')
         % Either URL is wrong, or the web service is down - warn the user
-        errordlg('Web service not available: %s', service_url);
+        errordlg('Whisper seg web service not available at: %s', service_url);
     else
         % Something else went wrong
         rethrow(ME);
     end
     % Return an empty array and move on
-    segments = zeros(0, 2);
+    segmentTimes = zeros(0, 2);
 end
