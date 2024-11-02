900b09e939a909dc899911f51f93e670ece73a21
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-11-02 13:14:58 -0400

    Added template for colormap plugins

---
+++ source/egc_Template.m
@@ -0,0 +1,15 @@
+function colorMap = egc_Template()
+% Template for creating ElectroGui color map functions
+%
+% Arguments:
+%   (None)
+%
+% Outputs:
+%   colorMap: A colormap
+% 
+% Modify the following code to create a new colormap function.
+% To make it available from within electro_gui, save it in the electro_gui
+% source code folder with a name beginning with 'egc_' and ending with '.m'
+
+% ADD YOUR CODE BELOW
+colorMap = [];
