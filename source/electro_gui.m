GoldbergLab/ElectroGui

commit d5038448a60e98006fac4988df0790a1c322eadf

Bugfix - click and drag zoom broken in top sound axes

source/electro_gui.m
@@ -9991,7 +9991,7 @@ end
                 %   it's just a click.
         
                 rect = rbbox();
-                rect = getFigureCoordsInAxesDataUnits(rect, obj.axes_Segments);
+                rect = getFigureCoordsInAxesDataUnits(rect, obj.axes_Sound);
 
                 if rect(3) == 0
                     % Click/drag box has zero width, so we're going to shift the zoom

commit ef90ee522921bb19be3aab87618db38406d9f4df

Fix click-and-drag-zoom in OTHER axes
d503844 accidentally broke zooming for all the other axes. Now it should work for all of them.

source/electro_gui.m
@@ -9979,7 +9979,7 @@ end
                 %   it's just a click.
         
                 rect = rbbox();
-                rect = getFigureCoordsInAxesDataUnits(rect, obj.axes_Sound);
+                rect = getFigureCoordsInAxesDataUnits(rect, hObject);
 
                 if rect(3) == 0
                     % Click/drag box has zero width, so we're going to shift the zoom