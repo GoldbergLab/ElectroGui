0600d514eb9172ca59e055eac32d558efe835b77
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-12-09 11:08:44 -0500

    egl_WaveRead_Stereo update
    Make loaddata input optional

--- source/egl_WaveRead_Stereo.m
+++ source/egl_WaveRead_Stereo.m
@@ -2,6 +2,10 @@ function [data, fs, dateandtime, label, props] = egl_WaveRead_Stereo(filename, l
 % ElectroGui file loader
 % Reads wavefiles, and loads as a 2D array if stereo data is present
 % Extracts date and time information from the file info
+arguments
+    filename
+    loaddata = true
+end
 
 if loaddata == 1
     [data, fs] = audioread(filename);
