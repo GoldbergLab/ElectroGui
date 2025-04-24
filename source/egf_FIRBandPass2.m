6460218010b265a781965504b66203e28728f784
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2025-04-24 15:26:46 -0400

    Update legacy egf_FIRBandPass2

--- source/egf_FIRBandPass2.m
+++ source/egf_FIRBandPass2.m
@@ -1,7 +1,7 @@
-function [snd, lab] = egf_FIRBandPass2(a, fs, params)
+function [snd, lab] = egf_FIRBandPass2(varargin)
 % ElectroGui filter
 
 % This is included for legacy purposes, and was only necessary in old 
 % versions of electro_gui.
 
-[snd, lab] = egf_FIRBandPass2(a, fs, params);
+[snd, lab] = egf_FIRBandPass(varargin{:});
