2ac9fc4e28e6d5b7dc6068ac54b3e874fad47ac3
Brian Kardon <brian.kardon@gmail.com>
2026-03-27 10:51:39 -0400

    Fix incorrect reference to dbase.Times

--- source/electro_gui.m
+++ source/electro_gui.m
@@ -3188,8 +3188,8 @@ function timestamp = getFileTime(obj, filenum, forceRead)
     if ~electro_gui.isDataLoaded(obj.dbase)
         error('No data loaded');
     end
-    if ~forceRead && obj.dbase.Time ~= 0
-        timestamp = obj.dbase.Time;
+    if ~forceRead && obj.dbase.Times ~= 0
+        timestamp = obj.dbase.Times;
     else
         filePath = fullfile(obj.dbase.PathName, obj.dbase.SoundFiles(filenum).name);
         loader = obj.dbase.SoundLoader;
